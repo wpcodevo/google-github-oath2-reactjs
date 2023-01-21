eb619d48168340a074f7ba63a251410e654ed696
CODEVO <contact@codevoweb.com>
2023-01-21 04:33:43 +0000

    updated

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -2,7 +2,6 @@ import { useLocation, useNavigate } from "react-router-dom";
 import GitHubLogo from "../assets/github.svg";
 import GoogleLogo from "../assets/google.svg";
 import { getGitHubUrl } from "../utils/getGithubUrl";
-import { getGoogleUrl } from "../utils/getGoogleUrl";
 import { object, string, TypeOf } from "zod";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -163,10 +162,14 @@ const LoginPage = () => {
             <a
               className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
               style={{ backgroundColor: "#3b5998" }}
-              href={getGoogleUrl(from)}
+              href=""
               role="button"
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
+              onClick={(e) => {
+                e.preventDefault();
+                alert("Only GitHub OAuth is allowed");
+              }}
             >
               <img
                 className="pr-2"
